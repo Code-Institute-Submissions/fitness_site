6039634d227aa04361bca54d890160bef04e17d7
claire mitchell <clairelouisemitchell1@gmail.com>
2017-10-03 12:37:44 +0100

    first commit

---
+++ hello/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def get_index(request):
+    # this is your new view
+   return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
